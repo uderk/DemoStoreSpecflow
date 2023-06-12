379ab7bd9ce3fb91db9bba640cf00a7016ea725d
Venislav Zdravkov <venislav.zdravkov@gmail.com>
2023-06-13 00:10:33 +0300

    Adding a set up method

---
+++ Drivers/WebDriverLibrary.cs
@@ -0,0 +1,69 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Safari;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoStoreSpecflowProject.Drivers
+{
+    internal class WebDriverLibrary
+    {
+        private readonly ScenarioContext scenarioContext;
+        private readonly IWebDriver driver;
+
+        public WebDriverLibrary (ScenarioContext scenarioContext, IWebDriver driver)
+        {
+            this.scenarioContext = scenarioContext;
+            this.driver = driver;
+        }
+
+        public IWebDriver Setup(string browserName)
+        {
+            dynamic capability = GetBrowserOptions(browserName);
+            IWebDriver driver;
+
+            if (browserName.Equals("firefox", StringComparison.OrdinalIgnoreCase))
+            {
+                driver = new FirefoxDriver(capability);
+            }
+            else if (browserName.Equals("chrome", StringComparison.OrdinalIgnoreCase))
+            {
+                driver = new ChromeDriver(capability);
+            }
+            else if (browserName.Equals("edge", StringComparison.OrdinalIgnoreCase))
+            {
+                driver = new EdgeDriver(capability);
+            }
+            else
+            {
+                // Add handling for other browsers if needed
+                throw new NotSupportedException($"Browser '{browserName}' is not supported.");
+            }
+
+            this.scenarioContext.Set(driver, "WebDriver");
+            return driver;
+        }
+
+
+        private dynamic GetBrowserOptions(string browserName)
+        {
+            switch (browserName.ToLower())
+            {
+                case "chrome":
+                    return new ChromeOptions();
+                case "firefox":
+                    return new FirefoxOptions();
+                case "Edge": 
+                    return new EdgeOptions();
+                default: return new ChromeOptions();
+            }
+
+        }
+    }
+}
